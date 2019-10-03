a395fd5261e1c1ea16192fcff8bf240d7b5a3f1d
unknown <you@example.com>
2019-10-03 21:00:01 +0530

    changes done for driver only

--- src/test/java/com/salesforce_Test_case/Test_salesforce_check.java
+++ src/test/java/com/salesforce_Test_case/Test_salesforce_check.java
@@ -6,16 +6,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Test_salesforce_check {
-@Test
-	public static void testcase_two()
-	{
+	@Test
+	public static void testcase_two() {
+		
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\Drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.salesforce.com/in/");
-		
-		driver.findElement(By.xpath("//a[@class='drawer-link-header has-child']//span[@class='linktitle'][contains(text(),'Products')]")).click();
-	    driver.findElement(By.xpath("//a[@class='drawer-link-header has-child']//span[@class='linktitle'][contains(text(),'Sales')]")).click();
+		System.out.println("link should be open");
+
+		driver.findElement(By.xpath(
+				"//a[@class='drawer-link-header has-child']//span[@class='linktitle'][contains(text(),'Products')]"))
+				.click();
+		System.out.println("products should be selected");
+		driver.findElement(By.xpath(
+				"//a[@class='drawer-link-header has-child']//span[@class='linktitle'][contains(text(),'Sales')]"))
+				.click();
 	}
-	
-	
+
 }
